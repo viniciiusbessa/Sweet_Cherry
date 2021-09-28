e3390389efbb72ce4ee31c0f50fc64a527c4dead
Gilson <gilsontorresr@hotmail.com>
2021-09-28 16:53:23 -0300

    src pronto

--- react/site/src/routes.js
+++ react/site/src/routes.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-function App() {
+import inicio from './pages/inicial/index'
+
+
+export default function Routes() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact={true} component={inicio} />
+      </Switch>
+    </BrowserRouter>
   );
 }
-
-export default App;
