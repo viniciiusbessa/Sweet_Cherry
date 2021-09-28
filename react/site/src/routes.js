bf0771bc6b4599847d4ad8de030b59299f8d1cf4
Laura <laurarfs18@gmail.com>
2021-09-28 18:20:11 -0300

    okok

--- react/site/src/routes.js
+++ react/site/src/routes.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import inicio from './pages/inicial/index'
+import CompraFinalizada from './pages/compra-finalizada'
 
 
 export default function Routes() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact={true} component={inicio} />
-      </Switch>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact={true} component={CompraFinalizada} />
+            </Switch>
+        </BrowserRouter>
+    )
 }
