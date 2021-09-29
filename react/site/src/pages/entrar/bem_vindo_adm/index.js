345faf9a3baecc0753b2f801c5d39500131c545d
Gilson <gilsontorresr@hotmail.com>
2021-09-29 15:00:42 -0300

    react login feito

---
+++ react/site/src/pages/entrar/bem_vindo_adm/index.js
@@ -0,0 +1,52 @@
+import { ContainerLoginAdm } from './styled'
+
+import { useHistory } from 'react-router-dom'
+
+export default function LoginAdm() {
+    const navigation = useHistory();
+
+    const inicioAdm = async () => {
+        navigation.push('/inicio-adm')
+    }
+
+    return (
+        <ContainerLoginAdm>
+            <div className="cabecalho-entrar-bem-vindo">
+                <div className="bemvindo-titulo">Bem-vindo(a)</div>
+                <div className="bemvindo-titulo1">Administrador</div>
+            </div>
+
+            <div className="conteudo-entrar-bv">
+
+                <div className="img-login"><img src="../../assets/images/icone-login.svg" alt="" /> </div>
+
+                <div className="inputs-entrarbv">
+                    <div className="box-input-email">
+                        <img src="../../assets/images/img-email-login.svg" alt="" />
+                        <input className="input-email" type="text" placeholder="e-mail" />
+                    </div>
+
+                    <div className="box-input-senha">
+                        <img src="../../assets/images/img-senha-login.svg" alt="" />
+                        <input className="input-senha" type="password" placeholder="password" />
+                    </div>
+
+                    <div className="box-input-usuario">
+                        <img src="../../assets/images/login-usuario-adm.svg" alt="" />
+                        <input className="input-senha" type="text" placeholder="id_usuario" />
+                    </div>
+                    
+                </div>
+
+                <div className="botoes-entrar-bv">
+
+                    <div className="botoes-um">
+                        <button onClick={inicioAdm} className="bt-entrar"> Entrar </button>
+                    </div>
+                    
+                </div>
+
+            </div>
+        </ContainerLoginAdm>
+    )
+}
---
+++ react/site/src/pages/entrar/bem_vindo_adm/styled.js
@@ -0,0 +1,142 @@
+import styled from 'styled-components'
+
+const ContainerLoginAdm = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    .cabecalho-entrar-bem-vindo {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        font-family: Pattaya;
+
+        background-image: url('/assets/images/fundo2.png');
+        background-repeat: no-repeat;
+        background-size: 100% 240px;
+
+        height: 240px;
+    }
+
+    .bemvindo-titulo{
+        font-size: 60px;
+        padding-top: 0.5em;
+    }
+
+    .bemvindo-titulo1 {
+        font-size: 60px;
+    }
+
+    .conteudo-entrar-bv {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        height: 100%;
+    }
+
+    .img-login img {
+        width: 100px;
+        height: auto;
+
+        margin-top: -2em;
+    }
+
+    .box-input-email img {
+        width: 25px;
+        height: auto;
+
+        margin: 0em -1.7em -0.4em 0em;
+
+        position: relative;
+    }
+
+    .box-input-senha img {
+        width: 25px;
+        height: auto;
+
+        margin: 0em -1.7em -0.5em 0em;
+
+        position: relative;
+    }
+
+    .box-input-usuario img {
+        width: 23px;
+        height: auto;
+
+        margin: 0em -1.7em -0.5em 0em;
+
+        position: relative;
+    }
+
+    .inputs-entrarbv {
+        display: flex;
+        flex-direction: column;
+
+        margin-top: 1em;
+    }
+
+    .input-email, .input-senha {
+        font-size: 17px;
+        font-family: Patua One;
+
+        border-top: none;
+        border-left: none;
+        border-right: none;
+        border-bottom: 2.5px solid #C4C4C4;
+
+        cursor: text;
+
+        padding: 1.5em 0.5em 0.3em 2em;
+
+        width: 280px;
+    }
+
+    .input-email::placeholder, .input-senha::placeholder {
+        text-align: center;
+        color: #BBC6C4;
+    }
+
+    input:focus {
+        box-shadow: 0 0 0 0;
+        outline: 0;
+    }
+
+
+    .botoes-entrarbv{
+       display: flex;
+       flex-direction: column;
+    }
+
+    .botoes-um {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+
+        margin: 3.5em 0em 1.5em 0em;
+    }
+
+    .botoes-um button {
+        font-family: Patua One;
+    }
+
+    .bt-entrar {
+        font-size: 17px;
+        color: #140018;
+
+        background-color: #D1EAF5;
+
+        border: none;
+        border-radius: 12px;
+
+        padding: 0.3em 1.8em;
+    }
+
+    .bt-entrar:hover {
+        background-color: #b5daeb;
+        cursor: pointer;
+        transition: 0.5s;
+    }
+`
+
+export { ContainerLoginAdm }
