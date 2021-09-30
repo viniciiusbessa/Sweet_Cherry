e3a555060b79ef989bec0c9e97f7cc2e4e099135
Letícia <leeh2003val@gmail.com>
2021-09-30 19:46:48 -0300

    componentes

---
+++ react/site/src/components/styled/buttons.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Botao = styled.button`
+    background-color: #D1EAF5;
+    font-family: Pacifico;
+    font-size: 18px;
+    border-radius: 10px;
+    border: none;
+    padding-right: 25px;
+    padding-left: 25px;
+`
+
+export {Botao}
---
+++ react/site/src/components/styled/inputs.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const InputEntrar = styled.input`
+    margin-bottom: 25px;
+    padding-top: 25px;
+    font-size: 20px;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    text-align: center;
+    font-family: Patua-one;
+`
+
+export {InputEntrar}
