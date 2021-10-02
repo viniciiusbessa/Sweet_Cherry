1e901de90bab9f356bed78d644b19df946b349aa
Laura <laurarfs18@gmail.com>
2021-10-02 16:02:35 -0300

    okok

--- react/site/src/pages/home/styled.js
+++ react/site/src/pages/home/styled.js
@@ -1,3 +1,4 @@
+
 import styled from "styled-components";
 
 const ContainerInicial = styled.div`
@@ -11,6 +12,52 @@ const ContainerInicial = styled.div`
     background-repeat: repeat-y;
 
 
+    .F1_home{
+        display: flex;
+        flex-direction: row;
+
+        justify-content: space-between;
+        margin-bottom: 30vh;
+        margin-top: 30vh;
+    }
+
+    .button1{
+        display: flex;
+        flex-direction: row-reverse;
+
+        border-bottom-right-radius: 2em;
+        border-top-right-radius: 2em;
+
+        background-color: lightpink;
+
+    }
+
+    .button2{
+        display: flex;
+        flex-direction: row;
+
+        border-bottom-left-radius: 2em;
+        border-top-left-radius: 2em;
+        
+        background-color: lightblue;
+
+    }
+
+    .button1, .button2{
+        cursor: pointer;
+
+        align-items: center;
+
+        border: none;
+        
+        width: 15em;
+        height: 5em;
+
+        animation-duration: 10s;
+    }
+
 `
+
+
 
 export {ContainerInicial}
