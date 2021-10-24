e48f195cbbc6c580b9af2865d584c3d74b137e0a
Gilson <gilsontorresr@hotmail.com>
2021-10-24 16:47:17 -0300

    ok

--- react/site/src/pages/home/index.js
+++ react/site/src/pages/home/index.js
@@ -39,64 +39,64 @@ export default function Inicial () {
             <Cabecalho/>
            
             <div className="F1_home">
-                    <div className="perfil">
-                        <button className="button1 animate__animated animate__bounceInLeft" onClick={perfil}><img src="/assets/images/pesquisa-de-usuario 1.png" alt=""/></button>
-                    </div>
-                
-                    <div className="busca">
-                        <button className="button2 animate__animated animate__bounceInRight" onClick={categorias}><img src="/assets/images/ferramenta-lupa 1.png" alt=""/></button>
-                    </div>
+                <div className="perfil">
+                    <button className="button1 animate__animated animate__bounceInLeft" onClick={perfil}><img src="/assets/images/pesquisa-de-usuario 1.png" alt=""/></button>
+                </div>
+            
+                <div className="busca">
+                    <button className="button2 animate__animated animate__bounceInRight" onClick={categorias}><img src="/assets/images/ferramenta-lupa 1.png" alt=""/></button>
+                </div>
             </div>
-                <BoxNews/>
-                <BoxSlide>
-                    <div className="Faixa3_inicio">
-                        <div className="titulo"> Destaques </div>
-                        <div className="boxSlide">
-                            <Splide
-                            options={ {
-                                direction: 'ttb',
-                                height   : '35rem',
-                                wheel    : true,
-                                type   : 'loop',
-                                drag   : 'free',
-                                } }
-                            >
-                                <SplideSlide>
-                                    <BoxProduto nome="Kit Kat Cake" preco="R$ 90,99" imagem="/assets/images/bolokitkat.jpg"/>   
-                                </SplideSlide>
-                                <SplideSlide>
-                                    <BoxProduto nome="Trufa" preco="R$ 6,80" imagem="/assets/images/trufa1.jpg"/>
-                                </SplideSlide>
-                                <SplideSlide>
-                                    <BoxProduto nome="Kit Kat Cake" preco="R$ 90,99" imagem="/assets/images/bolokitkat.jpg"/>   
-                                </SplideSlide>
-                        </Splide>
-                        </div>
-                        </div>
-                </BoxSlide>
-                   <BoxEmAlta>  
-                    <div className="Faixa4_inicio">
-                        <div className="titulo">Em alta</div>
-                        <div className="box">
-                        <div className="box_em_alta">
-                            <div className="imagem_emAlta">
-                                <img src="../../assets/images/macarrons.jpg" alt=""/>
-                            </div>
-                            <div className="box_texto">
-                                <p>Os Deliciosos Macarons chegaram!</p>
-                                <p>Delicie-se na sua cor favorita com esses biscoitos</p>
-                                <button onClick={categorias}> Garanta já o seu!  </button>
-                            </div>
-                        </div>
-                            <div className="container_doces">
-                                <BoxProduto nome="Sonho sabor chocolate" preco="R$ 9,99" imagem="/assets/images/sonho.jpg"/>
-                                <BoxProduto nome="Cupcake" preco="R$ 15,50" imagem="/assets/images/cupcakemorango.jpg"/>
+            <BoxNews/>
+            <BoxSlide>
+                <div className="Faixa3_inicio">
+                    <div className="titulo"> Destaques </div>
+                    <div className="boxSlide">
+                        <Splide
+                        options={ {
+                            direction: 'ttb',
+                            height   : '35rem',
+                            wheel    : true,
+                            type   : 'loop',
+                            drag   : 'free',
+                            } }
+                        >
+                            <SplideSlide>
+                                <BoxProduto nome="Kit Kat Cake" preco="R$ 90,99" imagem="/assets/images/bolokitkat.jpg"/>   
+                            </SplideSlide>
+                            <SplideSlide>
                                 <BoxProduto nome="Trufa" preco="R$ 6,80" imagem="/assets/images/trufa1.jpg"/>
-
-                            </div>
+                            </SplideSlide>
+                            <SplideSlide>
+                                <BoxProduto nome="Kit Kat Cake" preco="R$ 90,99" imagem="/assets/images/bolokitkat.jpg"/>   
+                            </SplideSlide>
+                        </Splide>
+                    </div>
+                </div>
+            </BoxSlide>
+            <BoxEmAlta>  
+                <div className="Faixa4_inicio">
+                    <div className="titulo">Em alta</div>
+                    <div className="box">
+                    <div className="box_em_alta">
+                        <div className="imagem_emAlta">
+                            <img src="../../assets/images/macarrons.jpg" alt=""/>
+                        </div>
+                        <div className="box_texto">
+                            <p>Os Deliciosos Macarons chegaram!</p>
+                            <p>Delicie-se na sua cor favorita com esses biscoitos</p>
+                            <button onClick={categorias}> Garanta já o seu!  </button>
                         </div>
                     </div>
-                </BoxEmAlta>
+                        <div className="container_doces">
+                            <BoxProduto nome="Sonho sabor chocolate" preco="R$ 9,99" imagem="/assets/images/sonho.jpg"/>
+                            <BoxProduto nome="Cupcake" preco="R$ 15,50" imagem="/assets/images/cupcakemorango.jpg"/>
+                            <BoxProduto nome="Trufa" preco="R$ 6,80" imagem="/assets/images/trufa1.jpg"/>
+
+                        </div>
+                    </div>
+                </div>
+            </BoxEmAlta>
             <Rodape/>
         </ContainerInicial>
     )
