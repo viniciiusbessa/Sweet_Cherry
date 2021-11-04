0be4b6710af76ae798bf6751975d4a52b4c09909
Gilson <gilsontorresr@hotmail.com>
2021-11-04 20:38:53 -0300

    ok

--- react/site/src/components/product/cardProduct/index.js
+++ react/site/src/components/product/cardProduct/index.js
@@ -7,12 +7,8 @@ import { useState } from "react";
 
 export default function BoxProduto (props){
     const navigation = useHistory();
-<<<<<<< HEAD
-    const [product] = useState([]);
-=======
-    const [product, setProduct] = useState(props.info);
->>>>>>> 558c43281654455cf705e784c6d09479715b4908
     
+    const [product] = useState(props.info);
     
     const compra = async () => {
         navigation.push('/compra')
