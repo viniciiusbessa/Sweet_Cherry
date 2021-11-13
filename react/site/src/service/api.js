22c8e4d647ec8b5362f11ee0746c77cf9ed3eb5a
João <joaopedroapparecido@gmail.com>
2021-11-13 13:56:54 -0300

    Validações ADM

--- react/site/src/service/api.js
+++ react/site/src/service/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 const api = axios.create({
-    baseURL: 'https://sweetcherryv9.herokuapp.com/'
+    // baseURL: 'https://sweetcherryv9.herokuapp.com/'
+    baseURL: 'http://localhost:3030/'
 })
 
 export default class Api {
