00309fcc53d727e23a7ddc9ff338a36058bc5ea2
Leticia <leeh2003val@gmail.com>
2021-11-15 01:01:05 -0300

    detalhes.

--- react/site/src/pages/home/box-news/index.js
+++ react/site/src/pages/home/box-news/index.js
@@ -19,7 +19,7 @@ export default function BoxNews (props) {
     const responsive = {
         desktop: {
           breakpoint: { max: 3000, min: 1024 },
-          items: 4
+          items: 3
         },
         tablet: {
           breakpoint: { max: 1024, min: 464 },
