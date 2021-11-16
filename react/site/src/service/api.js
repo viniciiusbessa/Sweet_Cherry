88c928b4e5f4d94a4bdfdafa5023210bd514e20f
vinibessa <vinibessa2005@gmail.com>
2021-11-16 00:00:23 -0300

    subiu pra interweb

--- react/site/src/service/api.js
+++ react/site/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const api = axios.create({
-    // baseURL: 'https://sweetcherryv9.herokuapp.com/'
-    baseURL: 'http://localhost:3030/'
+     baseURL: 'https://swapiv1.herokuapp.com/'
+    // baseURL: 'http://localhost:3030/'
 })
 
 export default class Api {
